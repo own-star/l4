28e3cb6cf4b1bccf4568a92065bb882fbd95d384
Taras J. Honcharuk <taras.honcharuk@gmail.com>
2016-03-24 20:10:01 +0200

    Add function get_by_date to emo

--- src/emo.erl
+++ src/emo.erl
@@ -1,12 +1,14 @@
 -module(emo).
--export([start/1,stop/0,put_emo/2,get_emo/1,custodian/0]).
+-export([start/1,stop/0,put_emo/2,get_emo/1,get_by_date/2,custodian/0]).
 
 %% Emo server v1.4
 
 start([{drop_interval,DrTime}]) ->
 	ok = timer:start(),
 	{ok, _} = timer:apply_interval(DrTime*1000, emo, custodian, []),
-	persons = ets:new(persons,[set,public,named_table]).
+	persons = ets:new(persons,[set,public,named_table]);
+start(_) ->
+	io:format("Wrong arguments.~nUse ~p:start([{drop_interval,Sec}])~n", [?MODULE]).
 
 stop() -> 
 	custodian(),
@@ -23,33 +25,47 @@ get_emo(Name) ->
 		[] -> 
 			no_tuple_found;
 		[{_,_,Exp}] when CTime > Exp -> 
-			ets:delete(persons,BName),
+%%			ets:delete(persons,BName),
 			no_tuple_found;
 		[{_,Status,_}] -> 
 			binary_to_list(Status)
 	end.
 
+get_by_date(DateFrom, DateTo) ->
+	FromSec = calendar:datetime_to_gregorian_seconds(DateFrom),
+	ToSec = calendar:datetime_to_gregorian_seconds(DateTo),
+	get_by_date(ets:first(persons), FromSec, ToSec, []).
+
+get_by_date('$end_of_table',_,_,Acc) ->
+	{ok, Acc};
+get_by_date(Name, FromSec, ToSec, Acc) ->
+	[{_, Status, Exp}] = ets:lookup(persons, Name),
+	if
+		Exp >= FromSec,	Exp =< ToSec ->
+			get_by_date(ets:next(persons,Name),FromSec,ToSec,[{binary_to_list(Name), binary_to_list(Status)}|Acc]);
+		true -> 
+			get_by_date(ets:next(persons,Name),FromSec,ToSec,Acc)
+	end.
+
 
 custodian() ->
 	CTime = calendar:datetime_to_gregorian_seconds({date(), time()}),
-%	FKey = ets:first(persons),
-%	io:format("Key: ~p Current time in seconds: ~p~n",[FKey,CTime]),
-	io:format("Key: Current time in seconds: ~p~n",[CTime]),
+	io:format("Current time in seconds: ~p~n",[CTime]),
 	custodian(ets:first(persons),CTime).
 
 custodian('$end_of_table',_) ->
 	clear_table_done;
-custodian(Key,CTime) ->
-	io:format("Current time in seconds: ~p Key: ~p~n",[CTime,Key]),
-	NKey = ets:next(persons, Key),
-	[{_,_,Exp}] = ets:lookup(persons,Key),
+custodian(Name,CTime) ->
+	io:format("Current time in seconds: ~p Name: ~p~n",[CTime,Name]),
+	NName = ets:next(persons, Name),
+	[{_,_,Exp}] = ets:lookup(persons,Name),
 		if
 		   	CTime > Exp ->
-				ets:delete(persons,Key);
+				ets:delete(persons,Name);
 			true -> true
 				
 		end,
-	custodian(NKey,CTime).
+	custodian(NName,CTime).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -71,7 +87,11 @@ get_emo_test() -> [
 				 ?_assert(get_emo("Homer") =:= ":-)"),
 				 ?_assert(get_emo("Moe") =:= ":-|")
 				].
-
+%get_by_date_test() -> [
+%				 CTime = calendar:datetime_to_gregorian_seconds({date(), time()}),
+%				 DateTo = calendar:gregorian_seconds_to_datetime(CTime + 30),
+%				 ?_assert(get_by_date({date(), time()}, DateTo) =:= {ok, [{"Homer",":-)"},{"Moe",":-|"}]})
+%				].
 stop_test() -> [
 				 ?_assert(stop() =:= true)
 				].
